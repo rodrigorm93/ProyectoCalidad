cc879174f5ef7785239a70e4d3cb40eedda75266
rodrigorm93 <33364100+rodrigorm93@users.noreply.github.com>
2018-10-03 20:40:51 -0300

    v

--- resources/views/notas/grado.blade.php
+++ resources/views/notas/grado.blade.php
@@ -7,7 +7,7 @@
         <div class = "col-lg-8 col-md-8 col-sm-8 col-xs-12">
             
           
-                <legend><h3>Cursos Disponibles: </h3></legend>
+                <legend><h3>Calcular Promedios: </h3></legend>
                 @if(count($errors) > 0)
                 <div class="alert alert-danger">
                     <ul>
