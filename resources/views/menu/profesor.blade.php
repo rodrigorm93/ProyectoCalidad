1f35ad481b6226bcfd12dc4982864bc2ce05acbc
rodrigorm93 <33364100+rodrigorm93@users.noreply.github.com>
2018-10-08 23:36:20 -0300

    vs

--- resources/views/menu/profesor.blade.php
+++ resources/views/menu/profesor.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cursos | Colegio </title>
+    <title>Materias | Colegio </title>
 
     <!-- Estilos CSS  -->
     {!!Html::style('profesor/css/extra.css')!!}
